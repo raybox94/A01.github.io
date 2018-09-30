37534f63c24ba08898a42d45701b6cf471bc4e2d
Rayaan Ahmed <raybox94@gmail.com>
2018-09-30 17:00:28 -0500

    Contact Validation

--- contactus.js
+++ contactus.js
@@ -10,6 +10,10 @@ let name = document.getElementById("name").value;
 let email = document.getElementById("email").value;
 let question = document.getElementById("question").value;
 
-alert("Name: "+name+"\nEmail: "+email+"\nQuestion: "+question);
-
+if(name === "" || email === "" || question === "")
+{
+    alert("You need to fill all fields")
+}else{
+        alert("Name: "+name+"\nEmail: "+email+"\nQuestion: "+question);
+}
 }//end of function
